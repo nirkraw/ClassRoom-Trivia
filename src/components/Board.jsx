fd5d393b11f7e7403f0b06dd7404e9ef469b0c8b
nirkraw <nirkaw@gmail.com>
2020-10-30 13:15:35 -0400

    fix extra option bug

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,7 +7,7 @@ import Clock from "./Clock";
 import song from "../assets/background-music.mp3";
 import mute from "../assets/mute.png";
 import unmute from "../assets/unmute.png";
-import cheer from "../assets/game-over.wav"
+import cheer from "../assets/game-over.wav";
 
 export default class Board extends Component {
   constructor(props) {
@@ -17,6 +17,7 @@ export default class Board extends Component {
       score: 0,
       musicPlaying: false,
       endGame: false,
+      options: [],
     };
 
     this.rightAnswer = this.rightAnswer.bind(this);
@@ -24,18 +25,20 @@ export default class Board extends Component {
   }
 
   componentDidMount() {
-     this.animate()
+    this.createOptions();
+    this.animate();
   }
 
   animate() {
-     const question = document.getElementById("question-title-container");
-     const clock = document.getElementById("option-and-clock-container");
-     question.classList.add("animate-opacity");
-     clock.classList.add("animate-opacity");
+    const question = document.getElementById("question-title-container");
+    const clock = document.getElementById("option-and-clock-container");
+    question.classList.add("animate-opacity");
+    clock.classList.add("animate-opacity");
   }
 
   nextQuestion() {
-    this.animate(); 
+    this.animate();
+    this.createOptions();
     const newIdx = this.state.questionIdx + 1;
     if (newIdx === Questions.length) {
       const audio = document.getElementById("cheer");
@@ -65,12 +68,13 @@ export default class Board extends Component {
     this.setState({ musicPlaying: false });
   };
 
-  getRandomizedOptions(currQuestion) {
-    const options = currQuestion.incorrect;
-    options.push(currQuestion.correct);
+  createOptions = () => {
+    const options = Questions[this.state.questionIdx].incorrect;
+    options.push(Questions[this.state.questionIdx].correct);
     options.sort(() => Math.random() - 0.5);
+    this.setState({ options });
     return options;
-  }
+  };
 
   restartGame = () => {
     this.setState({
@@ -79,17 +83,16 @@ export default class Board extends Component {
       musicPlaying: false,
       endGame: false,
     });
-  }
+  };
 
   render() {
-    const { endGame, score, questionIdx, musicPlaying } = this.state;
+    const { endGame, score, questionIdx, musicPlaying, options } = this.state;
 
-    const currQuestion = Questions[questionIdx];
-    const options = this.getRandomizedOptions(currQuestion).map((option, i) => (
+    const optionItems = options.map((option, i) => (
       <OptionItem
         option={option}
         key={i}
-        answer={currQuestion.correct}
+        answer={Questions[this.state.questionIdx].correct}
         rightAnswer={this.rightAnswer}
         nextQuestion={this.nextQuestion}
       />
@@ -101,7 +104,9 @@ export default class Board extends Component {
           <h1 id="final-score">
             Final Score: {score}/{questionIdx - 1}
           </h1>
-          <h1 onClick={this.restartGame} id="restart-game">Try Again?</h1>
+          <h1 onClick={this.restartGame} id="restart-game">
+            Try Again?
+          </h1>
         </div>
       );
 
@@ -115,11 +120,13 @@ export default class Board extends Component {
         <audio id="theme" src={song}></audio>
         <audio id="cheer" src={cheer}></audio>
         <div id="question-title-container">
-          <h1 id="question-text">{currQuestion.question}</h1>
+          <h1 id="question-text">
+            {Questions[this.state.questionIdx].question}
+          </h1>
         </div>
         <div id="option-and-clock-container">
           <div id="options-container">
-            <ul id="options-ul">{options}</ul>
+            <ul id="options-ul">{optionItems}</ul>
           </div>
           <div id="clock-container">
             <Clock nextQuestion={this.nextQuestion} />
